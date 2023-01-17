afcf33b3a9176bec81c86a88398bb3bab77f40d9
CleverLemming1337 <119337770+CleverLemming1337@users.noreply.github.com>
2023-01-17 08:48:26 +0100

    Update request.js

--- request.js
+++ request.js
@@ -1,6 +1,8 @@
 
 const userAction = async () => {
-  const response = await fetch('http://example.com/movies.json');
+  ean = 12345
+  const response = await fetch('http://opengtindb.org/?ean='+ean+'&cmd=query&queryid=400000000');
   const myJson = await response.json(); //extract JSON from the http response
   // do something with myJson
+  alert('myJson');
 }
